1012ee8b00c387d5be8ab51f8a1527361dbc37ba
Manu <42864020+manusheoran@users.noreply.github.com>
2022-05-10 21:39:33 +0530

    Update dataset_mapper.py

--- ubteacher/data/dataset_mapper.py
+++ ubteacher/data/dataset_mapper.py
@@ -79,7 +79,15 @@ class DatasetMapperTwoCropSeparate(DatasetMapper):
             dict: a format that builtin models in detectron2 accept
         """
         dataset_dict = copy.deepcopy(dataset_dict)  # it will be modified by code below
-        image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
+        
+        #image = utils.read_image(dataset_dict["file_name"], format=self.img_format)
+        im1 = np.load(dataset_dict["file_name"])
+        ims = [im1] + im1 + [im1]
+        ims = [im.astype(float) for im in ims]
+        im = cv2.merge(ims)
+        image = im.astype(np.float32, copy=False) 
+
+        
         utils.check_image_size(dataset_dict, image)
 
         if "sem_seg_file_name" in dataset_dict:
