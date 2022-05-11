d8861a11e298c0695724ead2073485600c83cdad
Manu <42864020+manusheoran@users.noreply.github.com>
2022-05-11 19:07:28 +0530

    Update build.py

--- ubteacher/data/build.py
+++ ubteacher/data/build.py
@@ -51,8 +51,6 @@ def divide_label_unlabel(
 
     for i in range(len(dataset_dicts)):
         if i in labeled_idx:
-            print(dataset_dicts[i],i)
-            print(end)
             label_dicts.append(dataset_dicts[i])
         else:
             unlabel_dicts.append(dataset_dicts[i])
